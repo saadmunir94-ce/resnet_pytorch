2e591fdf1530b0442e1a073cda50895ee1c39af3
Saad Munir <69633332+saadmunir94-ce@users.noreply.github.com>
2024-04-02 05:00:53 +0500

    Update docstrings of data.py

--- data.py
+++ data.py
@@ -12,74 +12,76 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.simplefilter("ignore")
 
+# mean and standard deviation of the solar cell RGB images in the training set across the three channels.
 train_mean = [0.59685254, 0.59685254, 0.59685254]
 train_std = [0.16043035, 0.16043035, 0.16043035]
 
 
 class ChallengeDataset(Dataset):
-
+    """
+    Custom dataset class for the solar cell challenge dataset.
+    
+    Attributes:
+        to_tensor (torchvision.transforms.Compose): Transformation to convert images to PyTorch tensors.
+        val_transform (torchvision.transforms.Compose): Transformation for validation dataset.
+        train_transform (torchvision.transforms.Compose): Transformation for training dataset.
+        images (pandas.DataFrame): Pandas DataFrame containing image paths.
+        labels (pandas.DataFrame): Pandas DataFrame containing labels where each row is a tuple.
+        mode (str): Dataset mode, either 'train' or 'val'.
+    """
+    
     def __init__(self, data, mode):
-        self.DF = data
-        self.mode = mode    # val or train
-        # Compose is the callable class which does chain of transformations on the data
+        """
+        Initializes the ChallengeDataset.
+
+        Parameters:
+            data (pandas.DataFrame): Pandas DataFrame containing image paths and labels where each row contains the image path and the tuple of labels.
+            mode (str): Dataset mode, either 'train' or 'val'.
+        """
+        self.mode = mode 
+        # Compose is the callable class which does chain of transformations on the data.
         self.to_tensor = tv.transforms.Compose([tv.transforms.ToTensor()])
 
         # Consider creating two different transforms based on whether you are in the training or validation dataset.
         self.val_transform = tv.transforms.Compose([tv.transforms.ToPILImage(), tv.transforms.ToTensor(),
                                                  tv.transforms.Normalize(mean=train_mean, std=train_std)])
-        self.train_transform = tv.transforms.Compose([tv.transforms.ToPILImage(), tv.transforms.RandomHorizontalFlip(),tv.transforms.ToTensor(), tv.transforms.Normalize(mean=train_mean, std=train_std), ])
+        self.train_transform = tv.transforms.Compose([tv.transforms.ToPILImage(), tv.transforms.RandomHorizontalFlip(),
+                                                      tv.transforms.ToTensor(), tv.transforms.Normalize(mean=train_mean, std=train_std)])
 
-        self.images= self.DF.iloc[:, 0]
-        self.labels = self.DF.iloc[:, 1:]
-        self.count = 0
-
-
+        self.images= data.iloc[:, 0]
+        self.labels = data.iloc[:, 1:]
+        
     def __len__(self):
-        #if self.mode == 'train':
-        return len(self.DF)
-        #if self.mode == 'val':
-        #    return len(self.images.index)
+        """
+        Gets the number of samples in the dataset.
 
-    """The file names and the corresponding labels are listed in the csv-file data.csv. 
-    Each row in the csv-file contains the path to an image in our dataset and two numbers indicating 
-    if the solar cell shows a 'crack' and if the solar cell can be considered 'inactive'"""
+        Returns:
+            int: Number of samples.
+        """
+        return len(self.images)
+        
+    def __getitem__(self, index):
+        """
+        Get a sample from the dataset.
 
-    def __getitem__(self, index): # used for accessing list items
+        Parameters:
+            index (int): Index of the sample to retrieve.
 
+        Returns:
+            tuple: Tuple containing the image and its corresponding labels.
+        """
         if self.mode == 'val':
-            #self.count += 1
-            #print(self.count)
             temp_img = imread(self.images.iloc[index])
             img = gray2rgb(temp_img)
             img = self.val_transform(img)
-            labels = self.to_tensor(np.asarray(self.labels.iloc[index]).reshape(1,2))
-            #img = self.to_tensor(img)
+            # the labels have a shape of (1, 2) because it is a multi-class problem (whether solar cell shows a crack and if it can be considered inactive).
+            labels = self.to_tensor(np.asarray(self.labels.iloc[index]).reshape(1, 2))
             return (img, labels)
         if self.mode == 'train':
-            #self.count += 1
-            #print(self.count)
             temp_img = imread(self.images.iloc[index])
             img = gray2rgb(temp_img)
-
             img = self.train_transform(img)
+            # the labels have a shape of (1, 2) because it is a multi-class problem (whether solar cell shows a crack and if it can be considered inactive).
             labels = self.to_tensor(np.asarray(self.labels.iloc[index]).reshape(1, 2))
-            #img = self.to_tensor(img)
             return (img, labels)
-
-
-
-'''
-csv_path = ''
-for root, _, files in os.walk('.'):
-    for name in files:
-        if name == 'data.csv':
-            csv_path = os.path.join(root, name)
-tab = pd.read_csv(csv_path, sep=';')
-temp = ChallengeDataset(tab, 'val')
-
-val_dl = t.utils.data.DataLoader(temp, batch_size=1)
-print(val_dl)
-for x,y in val_dl:
-    print(y)
-    break'''
 
